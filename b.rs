efc4627f90380fd7f08ed1ecfcada07255570eb8
rexim <reximkut@gmail.com>
2025-04-22 16:17:22 +0700

    characters -> strings

--- b.rs
+++ b.rs
@@ -72,7 +72,7 @@ unsafe fn display_token_kind_temp(token: c_long) -> *const c_char {
         CLEX_shreq      => c">>=".as_ptr(),
         CLEX_eqarrow    => c"=>".as_ptr(),
         CLEX_dqstring   => c"string literal".as_ptr(),
-        // NOTE: single quote characters are opt-in in stb_c_lexer.h (see STB_C_LEX_C_SQ_STRINGS)
+        // NOTE: single quote strings are opt-in in stb_c_lexer.h (see STB_C_LEX_C_SQ_STRINGS)
         CLEX_sqstring   => c"single quote literal".as_ptr(),
         CLEX_charlit    => c"character literal".as_ptr(),
         CLEX_intlit     => c"integer literal".as_ptr(),
